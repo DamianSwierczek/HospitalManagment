c13c19beede50c1cefe3dd3815d1030b03db5294
Damian Świerczek <damian.swierczek@microsoft.wsei.edu.pl>
2021-02-19 18:30:19 +0100

    load doctors done

---
+++ WindowWPF.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HospitalManagment
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WindowWPF.xaml
+    /// </summary>
+    public partial class WindowWPF : Window
+    {
+        public WindowWPF()
+        {
+            InitializeComponent();
+
+            HospitalManagementDBEntities db = new HospitalManagementDBEntities();
+
+            var docs = from d in db.Doctors
+                       select new
+                       {
+                           DoctorName = d.Name,
+                           Speciality = d.Specialization,
+                           Qualification = d.Qualification,
+                           Gender = d.Gender
+                       };
+
+            foreach (var item in docs)
+            {
+                Console.WriteLine(item.DoctorName);
+                Console.WriteLine(item.Speciality);
+                Console.WriteLine(item.Qualification);
+                Console.WriteLine(item.Gender);
+            }
+
+            this.gridDoctors.ItemsSource = docs.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            HospitalManagementDBEntities db = new HospitalManagementDBEntities();
+
+            Doctor doctorObject = new Doctor()
+            {
+                Name = "Dr Someone",
+                Qualification = "Some Qualification"
+
+
+            };
+
+            db.Doctors.Add(doctorObject);
+            db.SaveChanges();
+        }
+
+        private void BtnLoadDoctors_Click(object sender, RoutedEventArgs e)
+        {
+            HospitalManagementDBEntities db = new HospitalManagementDBEntities();
+
+            this.gridDoctors.ItemsSource = db.Doctors.ToList();
+        }
+    }
+}
