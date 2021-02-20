5302bb8963049c0674d97534777ea8fedbc09805
Damian Świerczek <damian.swierczek@microsoft.wsei.edu.pl>
2021-02-20 19:58:06 +0100

    added second dialogue and some text fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace HospitalManagment
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowWPF windowWPF = new WindowWPF();
+
+            windowWPF.ShowDialog();
+
+        }
     }
 }
